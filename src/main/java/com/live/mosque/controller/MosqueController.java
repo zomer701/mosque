de262c3e3f905e7be0442b6697d094d0185501e4
Mykhailo Markhain <mmarkhain@totalwine.com>
2024-07-05 23:15:24 -0600

    Put CRUD

--- src/main/java/com/live/mosque/controller/MosqueController.java
+++ src/main/java/com/live/mosque/controller/MosqueController.java
@@ -3,15 +3,7 @@ package com.live.mosque.controller;
 import com.live.mosque.data.Mosque;
 import com.live.mosque.service.MosqueService;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +14,9 @@ public class MosqueController {
     @Resource
     private MosqueService mosqueService;
 
-    @GetMapping
-    public List<Mosque> getAll() {
-        return mosqueService.getAllMosque();
+    @GetMapping("/all")
+    public List<Mosque> getAll(@RequestParam(defaultValue = "100") int count) {
+        return mosqueService.getAllMosque(count);
     }
 
     @GetMapping("/{uid}")
@@ -47,8 +39,8 @@ public class MosqueController {
         return mosqueService.putUpdate(mosque);
     }
 
-    @DeleteMapping
-    public void delete(String uid) {
+    @DeleteMapping("/{uid}")
+    public void delete(@PathVariable String uid) {
         mosqueService.delete(uid);
     }
 }
--- src/main/java/com/live/mosque/service/MosqueService.java
+++ src/main/java/com/live/mosque/service/MosqueService.java
@@ -3,13 +3,23 @@ package com.live.mosque.service;
 import com.live.mosque.data.Mosque;
 import com.live.mosque.item.MosquItem;
 import jakarta.annotation.Resource;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
+import software.amazon.awssdk.enhanced.dynamodb.model.PagePublisher;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 import static com.live.mosque.item.MosquItem.mosqueTableSchema;
@@ -21,8 +31,26 @@ public class MosqueService {
     @Resource
     private DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient;
 
-    public List<Mosque> getAllMosque() {
-        return List.of();
+    public List<Mosque> getAllMosque(int count) {
+        final DynamoDbAsyncTable<MosquItem> mosquItemDynamoDbAsyncTable =
+                dynamoDbEnhancedAsyncClient.table("mosque", mosqueTableSchema);
+
+        List<Mosque> resultData = new ArrayList<>();
+
+        mosquItemDynamoDbAsyncTable.scan().items().limit(count)
+                .subscribe(product -> {
+                      Mosque mosque = new Mosque();
+                      mosque.setUid(product.getPk());
+                      mosque.setMeta(product.getMeta());
+                    resultData.add(mosque);
+                })
+                .exceptionally(failure -> {
+                    log.error("ERROR  - ", failure);
+                    return null;
+                })
+                .join(); // If needed, blocks the subscribe() method thread until it is finished processing.
+
+        return resultData;
     }
 
     public void addMosque(Mosque mosque) {
@@ -32,8 +60,8 @@ public class MosqueService {
         try {
             MosquItem mosquItem = new MosquItem();
             mosquItem.setPk(mosque.getUid());
-            mosquItem.setSk(mosque.getUid());
-            mosquItem.setMeta(mosque.getMeta());
+            mosquItem.setSk("#");
+            mosquItem.setMeta(Optional.ofNullable(mosque.getMeta()).orElse(new HashMap<>()));
 
             mosquItemDynamoDbAsyncTable.putItem(mosquItem).get();
         } catch (InterruptedException | ExecutionException e) {
@@ -42,21 +70,64 @@ public class MosqueService {
     }
 
     public Mosque patchUpdate(Mosque mosque) {
-        return null;
+        final DynamoDbAsyncTable<MosquItem> mosquItemDynamoDbAsyncTable =
+                dynamoDbEnhancedAsyncClient.table("mosque", mosqueTableSchema);
+        try {
+            MosquItem item = mosquItemDynamoDbAsyncTable.getItem(Key.builder().partitionValue(mosque.getUid())
+                    .sortValue("#").build()).get();
+
+            item.setMeta(Optional.ofNullable(mosque.getMeta()).orElse(new HashMap<>()));
+            mosquItemDynamoDbAsyncTable.putItem(item).get();
+            return mosque;
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-
     public Mosque putUpdate(Mosque mosque) {
-        return null;
+        final DynamoDbAsyncTable<MosquItem> mosquItemDynamoDbAsyncTable =
+                dynamoDbEnhancedAsyncClient.table("mosque", mosqueTableSchema);
+        try {
+            MosquItem item = mosquItemDynamoDbAsyncTable.getItem(Key.builder().partitionValue(mosque.getUid())
+                    .sortValue("#").build()).get();
+            Map<String, Object> date = item.getMeta();
+
+            Map<String, Object> current = Optional.ofNullable(mosque.getMeta()).orElse(new HashMap<>());
+
+            for (String key: current.keySet()) {
+                date.put(key, current.get(key));
+            }
+
+            item.setMeta(date);
+
+            mosquItemDynamoDbAsyncTable.putItem(item).get();
+
+            mosque.setMeta(date);
+            return mosque;
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Mosque findByKey(String uid) {
-        return null;
+        final DynamoDbAsyncTable<MosquItem> mosquItemDynamoDbAsyncTable =
+                dynamoDbEnhancedAsyncClient.table("mosque", mosqueTableSchema);
+        try {
+            MosquItem item = mosquItemDynamoDbAsyncTable.getItem(Key.builder().partitionValue(uid).sortValue("#").build()).get();
+            Mosque mosque = new Mosque();
+            mosque.setUid(item.getPk());
+            mosque.setMeta(item.getMeta());
+
+            return mosque;
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    @SneakyThrows
     public void delete(String uid) {
         final DynamoDbAsyncTable<MosquItem> mosquItemDynamoDbAsyncTable =
                 dynamoDbEnhancedAsyncClient.table("mosque", mosqueTableSchema);
-        mosquItemDynamoDbAsyncTable.deleteItem(Key.builder().partitionValue(uid).build());
+        mosquItemDynamoDbAsyncTable.deleteItem(Key.builder().partitionValue(uid).sortValue("#").build()).get();
     }
 }
